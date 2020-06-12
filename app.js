6d01abd8fd10c7ac422a0d9c87c22613c1e65d3a
Gabriel <gabrielbarr811@gmail.com>
2020-06-12 12:02:08 +0100

    Inital Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Requirements
+const   express         = require("express"),
+        app             = express(),
+        bodyParser      = require("body-parser");
+        
+
+// App Set-Up
+
+
+
+
+// RESTful Routes
+
+app.get("/", function(req, res){
+    res.send("Home Page")
+})
+
+
+
+
+
+app.listen(3000, function(){
+	console.log("gabrielbarr running");
+});
