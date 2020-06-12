13f67bcc13726ebc9334b534a87b68aca4dd010f
Gabriel <gabrielbarr811@gmail.com>
2020-06-12 18:05:08 +0100

    Basics

--- app.mjs
+++ app.mjs
@@ -9,16 +9,34 @@ const app = express();
 
 
 // App Set-Up
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 
 
 
 // RESTful Routes
 
+// Home Page
 app.get("/", function(req, res){
-    res.send("Home Page")
+    res.render("index")
+});
+
+// Projects Page
+app.get("/projects", function(req, res){
+    res.render("projects")
+});
+
+// About Page
+app.get("/about", function(req, res){
+    res.render("about")
+});
+
+// Contact Page
+app.get("/contact", function(req, res){
+    res.render("contact")
 })
-
-
 
 
 
