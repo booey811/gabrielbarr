e40f687c30652dad84d8d51503a3ed2477aca91b
Gabriel <gabrielbarr811@gmail.com>
2020-06-16 18:21:18 +0100

    40%

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,10 @@
+const glideConfig = {
+    type: "carousel",
+    autoplay: 5000
+}
+
 $(document).ready(function(){
     $('.ui.dropdown.item').dropdown()
+    new Glide('.glide', glideConfig).mount()
 });
+
